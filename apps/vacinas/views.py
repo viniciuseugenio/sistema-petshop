dfe6d66eb68cbbb52a31606f7d6433e2adb653f2
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-02-11 09:29:46 -0300

    feat(registro-vacinas): add filtragem de registros por pet id

--- apps/vacinas/views.py
+++ apps/vacinas/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
 from .models import RegistroVacina, Vacina
 from .serializers import RegistroVacinaSerializer, VacinaSerializer
@@ -12,3 +13,13 @@ class VacinaViewSet(ModelViewSet):
 class RegistroVacinaViewSet(ModelViewSet):
     queryset = RegistroVacina.objects.all()
     serializer_class = RegistroVacinaSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        pet_id = request.query_params.get("pet")
+
+        if pet_id:
+            queryset = queryset.filter(pet__id=pet_id)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
