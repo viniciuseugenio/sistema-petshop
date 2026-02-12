55cd7656e18eb05b700a1fd691267621b03896a7
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-02-12 09:21:45 -0300

    feat(pets): add permissões e filtragem automática em viewset

--- apps/pets/views.py
+++ apps/pets/views.py
@@ -1,6 +1,8 @@
+from rest_framework.permissions import IsAdminUser
 from rest_framework.viewsets import ModelViewSet
 
-from apps.permissions import IsVeterinario, IsVeterinarioOrTutor
+from apps.permissions import IsVeterinarioOrTutor
+from apps.tutores.models import Tutor
 
 from . import serializers
 from .models import Pet
@@ -8,10 +10,16 @@ from .models import Pet
 
 class PetViewSet(ModelViewSet):
     def get_permissions(self):
-        if self.action == "list":
-            return [IsVeterinario()]
+        if self.action == "destroy":
+            return [IsAdminUser()]
 
         return [IsVeterinarioOrTutor()]
+
+    """
+    É possível filtar os pets por tutor através de query params.
+    Caso o usuário seja um tutor mas não veterinário, esse endpoint retorna somente
+    os pets do usuário em si.
+    """
 
     def get_queryset(self):
         queryset = Pet.objects.select_related("tutor", "tutor__user")
@@ -20,7 +28,12 @@ class PetViewSet(ModelViewSet):
         if tutor_id:
             queryset = queryset.filter(tutor__id=tutor_id)
 
-        return queryset
+        is_vet = self.request.user.groups.filter(name="veterinarios").exists()
+        if is_vet:
+            return queryset
+
+        tutor = Tutor.objects.get(user=self.request.user)
+        return queryset.filter(tutor=tutor)
 
     def get_serializer_class(self):
         if self.action == "create":
